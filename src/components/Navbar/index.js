55f55a894e0b87d96282a1d6c95a52e9539b046c
unknown <alleyk@gmail.com>
2022-10-13 17:51:27 -0700

    First commit

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { FaBars } from "react-icons/fa";
+import LogoSrc from "../../assets/images/ajlogo.png";
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  Logo,
+} from "./NavbarElements";
+
+const Navbar = ({ toggle }) => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">
+            <Logo src={LogoSrc} />
+          </NavLogo>
+          <MobileIcon onClick={toggle}>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about">About</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="skills">Skills</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="resume">Resume</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="contct">Contact</NavLinks>
+            </NavItem>
+          </NavMenu>
+        </NavbarContainer>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
